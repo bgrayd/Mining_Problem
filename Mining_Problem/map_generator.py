d64f45329fd1bdced2f08de404715c5e42b0c4eb
girrig <girrig3@gmail.com>
2016-04-16 17:30:17 -0500

    Added map generator

---
+++ Mining_Problem/map_generator.py
@@ -0,0 +1,64 @@
+'''
+Created on Feb 8, 2016
+
+@author: Brandon
+'''
+
+import numpy
+
+elements = ['0', '1']
+prob = [0.9, 0.1]
+
+map_file = open('input.txt', 'w')
+
+for l in xrange(100):
+    map = {}
+    
+    for i in xrange(8): #row
+        for j in xrange(8): #column
+            try:
+                if map[str(i),str(j)]:
+                    pass
+            except:
+                if numpy.random.choice(elements, p=prob) == '0':
+                    map[str(i),str(j)] = 0
+                    prob = [0.9, 0.1]
+                else:
+                    map[str(i),str(j)] = 1
+                    prob[0] = prob[0]/2
+                    prob[1] = 1 - prob[0]
+                    try:
+                        if map[str(i-1),str(j)]:
+                            if numpy.random.choice(elements, p=prob) == '1':
+                                map[str(i-1),str(j)] = 1
+                    except:
+                        pass
+                    try:
+                        if map[str(i+1),str(j)]:
+                            if numpy.random.choice(elements, p=prob) == '1':
+                                map[str(i-1),str(j)] = 1
+                    except:
+                        pass
+                    try:
+                        if map[str(i),str(j-1)]:
+                            if numpy.random.choice(elements, p=prob) == '1':
+                                map[str(i-1),str(j)] = 1
+                    except:
+                        pass
+                    try:
+                        if map[str(i),str(j+1)]:
+                            if numpy.random.choice(elements, p=prob) == '1':
+                                map[str(i-1),str(j)] = 1
+                    except:
+                        pass
+                prob = [0.9, 0.1]
+    
+    print map
+    
+    # export map
+    for i in xrange(8): #row
+        for j in xrange(8): #column
+            map_file.write(str(map[str(i),str(j)]))
+        map_file.write('\n')
+    
+    map_file.write("\n")
